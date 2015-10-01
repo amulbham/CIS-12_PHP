c49c1885f51d17e786a8784e7e8b5a5e85324cb8
Amul Bham <amul.bham@gmail.com>
2015-09-30 18:18:35 -0700

    functions and arrays

--- hmwrk/Assignment_5/Function_arrays.php
+++ hmwrk/Assignment_5/Function_arrays.php
@@ -22,44 +22,17 @@ and open the template in the editor.
     </head>
    
     <body>
-        <h1>Electro Magnetic Spectrum -1 Dimensional Arrays</h1>
+        <h1>Electro Magnetic Spectrum -Functions and Arrays</h1>
         <?php
             //Declare Variables
-                $rows = 16;
                 $waveN = array();
                 $wave = array(); //array to store the wave length   
-                $start = 3; 
                 $rowN = array();
-                
-            //Intialize arrays with column data
-            for($i =0; $i<$rows;$i++){
-                $rowN[$i] = $i+1;
-                $wave[$i] = "10^".$start;
-                //Column 3
-                        if ($start>-2){
-                            $waveN[$i]= 'Radio Waves';
-                        }
-                        else if ($start<=-2 and $start>-5){
-                            $waveN[$i]= 'Microwaves';
-                        }
-                        else if ($start<=-5 and $start>-6){
-                            $waveN[$i]= 'Infrared';
-                        }
-                        else if ($start<=-6 and $start>-8){
-                            $waveN[$i]= 'Visible';
-                        }
-                        else if ($start<=-8 and $start>-10){
-                            $waveN[$i]= 'Ultraviolet';
-                        }
-                        else if ($start<=-10 and $start>-12){
-                            $waveN[$i]= 'X-ray';
-                        }
-                        else{
-                            $waveN[$i]= 'Gamma Rays';
-                        }
-                $start--;
-            }
+                $start = 3; 
+                $rows = 16;
            
+            //Call the calculate array funciton
+            calArr($wave, $waveN, $rowN, $start, $rows);    
             //Create headings in the table
             echo '<table width="300" border="1">';
                 echo '<tr><th>Row Number</th><th>Wave Length(Meters)</th>
@@ -88,6 +61,43 @@ and open the template in the editor.
         
         ?>
         
+        
+        <?php
+         function calArr(&$wave,&$waveN,&$rowN,$start,$rows){    
+               
+            //Intialize arrays with column data
+            for($i =0; $i<$rows;$i++){
+                
+                $rowN[$i] = $i+1;
+                $wave[$i] = "10^".$start;
+                //Column 3
+                        if ($start>-2){
+                            $waveN[$i]= 'Radio Waves';
+                        }
+                        else if ($start<=-2 and $start>-5){
+                            $waveN[$i]= 'Microwaves';
+                        }
+                        else if ($start<=-5 and $start>-6){
+                            $waveN[$i]= 'Infrared';
+                        }
+                        else if ($start<=-6 and $start>-8){
+                            $waveN[$i]= 'Visible';
+                        }
+                        else if ($start<=-8 and $start>-10){
+                            $waveN[$i]= 'Ultraviolet';
+                        }
+                        else if ($start<=-10 and $start>-12){
+                            $waveN[$i]= 'X-ray';
+                        }
+                        else{
+                            $waveN[$i]= 'Gamma Rays';
+                        }
+                $start--;
+            }
+            }
+        
+        
+        ?>
     </body>
 </html>
 
